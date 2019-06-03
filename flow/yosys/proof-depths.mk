4e58600b96d1081cd0aaaf83af4d384c17de136e
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-06-03 14:25:51 +0100

    Tune proof depths for pclmul
     On branch dev/palu-rewrite
     Changes to be committed:
    	modified:   flow/yosys/proof-depths.mk

--- flow/yosys/proof-depths.mk
+++ flow/yosys/proof-depths.mk
@@ -17,8 +17,8 @@ $(SMTDIR)/instr_pmul_h_pw2.cov : BMC_STEPS=21
 $(SMTDIR)/instr_pmul_h_pw1.rpt : BMC_STEPS=37
 $(SMTDIR)/instr_pmul_h_pw1.cov : BMC_STEPS=37
 
-$(SMTDIR)/instr_pmul_l.rpt     : BMC_STEPS=13
-$(SMTDIR)/instr_pmul_l.cov     : BMC_STEPS=13
+$(SMTDIR)/instr_pmul_l.rpt     : BMC_STEPS=16
+$(SMTDIR)/instr_pmul_l.cov     : BMC_STEPS=16
 
 $(SMTDIR)/instr_pmul_l_pw2.rpt : BMC_STEPS=21
 $(SMTDIR)/instr_pmul_l_pw2.cov : BMC_STEPS=21
@@ -26,8 +26,8 @@ $(SMTDIR)/instr_pmul_l_pw2.cov : BMC_STEPS=21
 $(SMTDIR)/instr_pmul_l_pw1.rpt : BMC_STEPS=37
 $(SMTDIR)/instr_pmul_l_pw1.cov : BMC_STEPS=37
 
-$(SMTDIR)/instr_pclmul_h.rpt     : BMC_STEPS=13
-$(SMTDIR)/instr_pclmul_h.cov     : BMC_STEPS=13
+$(SMTDIR)/instr_pclmul_h.rpt     : BMC_STEPS=16
+$(SMTDIR)/instr_pclmul_h.cov     : BMC_STEPS=16
 
 $(SMTDIR)/instr_pclmul_h_pw2.rpt : BMC_STEPS=21
 $(SMTDIR)/instr_pclmul_h_pw2.cov : BMC_STEPS=21
@@ -35,14 +35,31 @@ $(SMTDIR)/instr_pclmul_h_pw2.cov : BMC_STEPS=21
 $(SMTDIR)/instr_pclmul_h_pw1.rpt : BMC_STEPS=37
 $(SMTDIR)/instr_pclmul_h_pw1.cov : BMC_STEPS=37
 
-$(SMTDIR)/instr_pclmul_l.rpt     : BMC_STEPS=13
-$(SMTDIR)/instr_pclmul_l.cov     : BMC_STEPS=13
+$(SMTDIR)/instr_pclmul_l.rpt     : BMC_STEPS=16
+$(SMTDIR)/instr_pclmul_l.cov     : BMC_STEPS=16
 
 $(SMTDIR)/instr_pclmul_l_pw2.rpt : BMC_STEPS=21
 $(SMTDIR)/instr_pclmul_l_pw2.cov : BMC_STEPS=21
 
 $(SMTDIR)/instr_pclmul_l_pw1.rpt : BMC_STEPS=37
 $(SMTDIR)/instr_pclmul_l_pw1.cov : BMC_STEPS=37
+
+$(SMTDIR)/instr_padd.cov        : BMC_STEPS=6
+$(SMTDIR)/instr_prot_i.cov      : BMC_STEPS=6
+$(SMTDIR)/instr_prot.cov        : BMC_STEPS=6
+$(SMTDIR)/instr_psll_i.cov      : BMC_STEPS=6
+$(SMTDIR)/instr_psll.cov        : BMC_STEPS=6
+$(SMTDIR)/instr_psrl_i.cov      : BMC_STEPS=6
+$(SMTDIR)/instr_psrl.cov        : BMC_STEPS=6
+$(SMTDIR)/instr_psub.cov        : BMC_STEPS=6
+$(SMTDIR)/instr_padd.rpt        : BMC_STEPS=6
+$(SMTDIR)/instr_prot_i.rpt      : BMC_STEPS=6
+$(SMTDIR)/instr_prot.rpt        : BMC_STEPS=6
+$(SMTDIR)/instr_psll_i.rpt      : BMC_STEPS=6
+$(SMTDIR)/instr_psll.rpt        : BMC_STEPS=6
+$(SMTDIR)/instr_psrl_i.rpt      : BMC_STEPS=6
+$(SMTDIR)/instr_psrl.rpt        : BMC_STEPS=6
+$(SMTDIR)/instr_psub.rpt        : BMC_STEPS=6
 
 $(SMTDIR)/instr_aesmix_dec.rpt      : BMC_STEPS=10
 $(SMTDIR)/instr_aesmix_enc.rpt      : BMC_STEPS=10

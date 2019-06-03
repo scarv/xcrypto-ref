30d0a5e0f8cf26e967c4452e871a6fc952a416b7
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-06-03 13:26:44 +0100

    First draft of re-implementing packed ALU
     On branch dev/palu-rewrite
     Changes to be committed:
    	modified:   rtl/coprocessor/scarv_cop_palu.v
    	modified:   rtl/coprocessor/scarv_cop_palu_adder.v
    	modified:   rtl/coprocessor/scarv_cop_palu_multiplier.v
    	modified:   rtl/coprocessor/scarv_cop_palu_shifter.v
    
     Changes not staged for commit:
    	modified:   extern/texmf (modified content)

--- rtl/coprocessor/scarv_cop_palu_adder.v
+++ rtl/coprocessor/scarv_cop_palu_adder.v
@@ -10,58 +10,117 @@
 // 
 
 //
-// module scarv_cop_palu_adder
+// module: p_addsub
 //
+//  Implemented packed addition/subtraction for 32-bit 2s complemente values.
 //
-module scarv_cop_palu_adder (
-    input  wire [31:0] a ,  // LHS input
-    input  wire [31:0] b ,  // RHS input
-    input  wire [ 2:0] pw,  // Current operation pack width
-    input  wire        sub, // Do subtract instead of add.
-    input  wire        ci,  // Carry in
-    output wire [31:0] c ,  // Result
-    output wire        co   // Carry out
+module p_addsub (
+
+input  wire [31:0]  lhs             , // Left hand input
+input  wire [31:0]  rhs             , // Right hand input.
+
+input  wire [ 4:0]  pw              , // Pack width to operate on
+input  wire [ 0:0]  sub             , // Subtract if set, else add.
+
+output wire [31:0]  c_out           , // Carry bits
+output wire [31:0]  result            // Result of the operation
+
 );
 
-`include "scarv_cop_common.vh"
+//
+// One-hot pack width wires
+wire pw_32 = pw[0];
+wire pw_16 = pw[1];
+wire pw_8  = pw[2];
+wire pw_4  = pw[3];
+wire pw_2  = pw[4];
 
+// Carry bit masks
+(*keep*)
+wire [31:0] carry_mask;
+wire [32:0] carry_chain;
+
+// Carry in IFF subtracting.
+assign      carry_chain[0] = sub;
+
+// Invert RHS iff subtracting.
+wire [31:0] rhs_m          = sub ? ~rhs : rhs;
+
+//
+// Generate the carry mask bits.
+assign carry_mask[ 0] = 1'b1;
+assign carry_mask[ 1] = !pw_2;
+assign carry_mask[ 2] = 1'b1;
+assign carry_mask[ 3] = !pw_2 && !pw_4;
+assign carry_mask[ 4] = 1'b1;
+assign carry_mask[ 5] = !pw_2;
+assign carry_mask[ 6] = 1'b1;
+assign carry_mask[ 7] = !pw_2 && !pw_4 && !pw_8;
+assign carry_mask[ 8] = 1'b1;
+assign carry_mask[ 9] = !pw_2;
+assign carry_mask[10] = 1'b1;
+assign carry_mask[11] = !pw_2 && !pw_4;
+assign carry_mask[12] = 1'b1;
+assign carry_mask[13] = !pw_2;
+assign carry_mask[14] = 1'b1;
+assign carry_mask[15] = !pw_2 && !pw_4 && !pw_8 && !pw_16;
+assign carry_mask[16] = 1'b1;
+assign carry_mask[17] = !pw_2;
+assign carry_mask[18] = 1'b1;
+assign carry_mask[19] = !pw_2 && !pw_4;
+assign carry_mask[20] = 1'b1;
+assign carry_mask[21] = !pw_2;
+assign carry_mask[22] = 1'b1;
+assign carry_mask[23] = !pw_2 && !pw_4 && !pw_8;
+assign carry_mask[24] = 1'b1;
+assign carry_mask[25] = !pw_2;
+assign carry_mask[26] = 1'b1;
+assign carry_mask[27] = !pw_2 && !pw_4;
+assign carry_mask[28] = 1'b1;
+assign carry_mask[29] = !pw_2;
+assign carry_mask[30] = 1'b1;
+assign carry_mask[31] = !pw_2 && !pw_4 && !pw_8 && !pw_16;
+
+//
+// Generate full adders, where carry in for each one is masked by
+// the corresponding carry mask bit.
 genvar i;
-
-wire [31:0] fci        ; // Force carry in high - for subtract
-wire [31:0] result_bits;
-wire [32:0] carry_bits;
-wire [32:0] carry_msk ;
-assign carry_msk[32] = 1'b0;
-
-assign carry_bits[0] = ci;
-assign co            = carry_bits[32];
-assign c             = result_bits[31:0];
-
-wire   pw_1          = pw == SCARV_COP_PW_1 ; // 1  32-bit word
-wire   pw_2          = pw == SCARV_COP_PW_2 ; // 2  16-bit halfwords
-wire   pw_4          = pw == SCARV_COP_PW_4 ; // 4   8-bit bytes
-wire   pw_8          = pw == SCARV_COP_PW_8 ; // 8   4-bit nibbles
-wire   pw_16         = pw == SCARV_COP_PW_16; // 16  2-bit crumbs
-
-wire [4:0]   pw_1hot       = {
-    pw_1                                  , 
-    pw_2                          || pw_1 ,
-    pw_4                  || pw_2 || pw_1 ,
-    pw_8          || pw_4 || pw_2 || pw_1 ,
-    pw_16 || pw_8 || pw_4 || pw_2 || pw_1 
-};
-
 generate for(i = 0; i < 32; i = i + 1) begin
 
-    wire [1:0] ir   = a[i]                             +
-                      (sub    ? !b[i] :  b[i]        ) +
-                      (fci[i] ? 1'b1  : carry_bits[i]) ;
-    assign fci[i] = i == 0 ? 1'b0   :
-                    sub && !carry_msk[i] && carry_msk[i-1];
+    wire   c_in     = carry_chain[i];
+    assign c_out[i] = (lhs[i] && rhs_m[i]) || (c_in && (lhs[i]^rhs_m[i]));
+    
+    wire   force_carry = sub && (
+        (i == 15 && pw_16) ||  
+        (i ==  7 && pw_8 ) ||
+        (i == 15 && pw_8 ) ||
+        (i == 23 && pw_8 ) ||
+        (i ==  3 && pw_4 ) ||
+        (i ==  7 && pw_4 ) ||
+        (i == 11 && pw_4 ) ||
+        (i == 15 && pw_4 ) ||
+        (i == 19 && pw_4 ) ||
+        (i == 23 && pw_4 ) ||
+        (i == 27 && pw_4 ) ||
+        (i ==  1 && pw_2 ) ||
+        (i ==  3 && pw_2 ) ||
+        (i ==  5 && pw_2 ) ||
+        (i ==  7 && pw_2 ) ||
+        (i ==  9 && pw_2 ) ||
+        (i == 11 && pw_2 ) ||
+        (i == 13 && pw_2 ) ||
+        (i == 15 && pw_2 ) ||
+        (i == 17 && pw_2 ) ||
+        (i == 19 && pw_2 ) ||
+        (i == 21 && pw_2 ) ||
+        (i == 23 && pw_2 ) ||
+        (i == 25 && pw_2 ) ||
+        (i == 27 && pw_2 ) ||
+        (i == 29 && pw_2 ) );
 
-    assign carry_msk[i]     = (i & pw_1hot) != 0 || i == 0;
-    assign result_bits[i]   = ir[0];
-    assign carry_bits [i+1] = ir[1] && carry_msk[i+1];
+    assign carry_chain[i+1] = (c_out[i] && carry_mask[i]) || force_carry;
+
+    assign result[i] = lhs[i] ^ rhs_m[i] ^ c_in;
 
 end endgenerate
 
